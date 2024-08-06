d476a8bd81d4895ff5ba9cc2236ae4cb5b312930
phuvinhnguyen <phuvinhnguyen812002@gmail.com>
2024-08-06 17:42:56 +0700

    dataset fix

--- kaggle/label_indexing.py
+++ kaggle/label_indexing.py
@@ -2,9 +2,9 @@ import pandas as pd
 import os
 
 dataset_file = os.path.join(os.path.dirname(
-    __file__), '../preprocess/Detection_NIH_1024.csv')
+    __file__), '../MedicalVision/preprocess/Detection_NIH_1024.csv')
 destination_data_file = os.path.join(os.path.dirname(
-    __file__), '../preprocess/Detection_NIH_1024_version_2.csv')
+    __file__), '../MedicalVision/preprocess/Detection_NIH_1024_version_2.csv')
 
 if __name__ == '__main__':
     data = pd.read_csv(dataset_file)
@@ -12,9 +12,14 @@ if __name__ == '__main__':
     data['Label'] = data['Finding Label'].map(label_map)
 
     data['bbox'] = data[['Bbox [x', 'y', 'w', 'h]']].values.tolist()
-    data = data.groupby('Image Index').agg(
-        lambda x: [i for i in x]).reset_index()
+    
+    # image_path of this dataframe should be agg as first
+    data = data.groupby('Image Index').agg({
+        'Label': list,
+        'bbox': list,
+        'image_path': 'first'  # Assuming that image_path is consistent for each Image Index
+    }).reset_index()
 
-    data = data[['Image Index', 'Label', 'bbox']]
+    data = data[['Image Index', 'Label', 'bbox', 'image_path']]
 
     data.to_csv(destination_data_file)

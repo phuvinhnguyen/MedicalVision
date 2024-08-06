1526f9da6792d179fd82d9a215f51eb4e2c7e7e8
phuvinhnguyen <phuvinhnguyen812002@gmail.com>
2024-08-06 17:05:20 +0700

    minor fix

--- MedicalVision/Dataset/Detection/__init__.py
+++ MedicalVision/Dataset/Detection/__init__.py
@@ -1,5 +1,5 @@
-import NIHDataset
+from .NIHDataset import get_dataset as NIHDataset
 
 EXISTING_DATASET = {
-    'NIH': NIHDataset.get_dataset
+    'NIH': NIHDataset
 }
--- MedicalVision/Examples/DETR_NIH.py
+++ MedicalVision/Examples/DETR_NIH.py
@@ -1,11 +1,11 @@
 from ..Setup.Detection.Setup import Runner
 
 
-def run(hf_id, token=None):
+def run(hf_id, token=None, dataset=()):
     runner = Runner(
         'facebook/detr-resnet-50',
         hf_repo_id=hf_id,
-        token=token, dataset=('NIH',),
+        token=token, dataset=('NIH', dataset),
         num_labels=8
     )
 

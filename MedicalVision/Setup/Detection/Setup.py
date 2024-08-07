d73978f68aef65a8d56340e0abb085249ba3832e
phuvinhnguyen <phuvinhnguyen812002@gmail.com>
2024-08-07 08:43:30 +0700

    visualization

--- MedicalVision/Setup/Detection/Setup.py
+++ MedicalVision/Setup/Detection/Setup.py
@@ -3,7 +3,27 @@ from .ModelWrapper import GeneralDetectionModel
 from pytorch_lightning import Trainer
 from ...Dataset.Detection import EXISTING_DATASET
 from transformers import AutoModelForObjectDetection, AutoProcessor
+import matplotlib.pyplot as plt
+import torch
+from torchvision.transforms import ToPILImage
 
+# colors for visualization
+COLORS = [[0.000, 0.447, 0.741], [0.850, 0.325, 0.098], [0.929, 0.694, 0.125],
+          [0.494, 0.184, 0.556], [0.466, 0.674, 0.188], [0.301, 0.745, 0.933]]
+
+def plot_results(pil_img, scores, labels, boxes):
+    plt.figure(figsize=(16,10))
+    plt.imshow(pil_img)
+    ax = plt.gca()
+    colors = COLORS * 100
+    for score, label, (xmin, ymin, xmax, ymax),c  in zip(scores.tolist(), labels.tolist(), boxes.tolist(), colors):
+        ax.add_patch(plt.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin,
+                                   fill=False, color=c, linewidth=3))
+        text = f'{model.config.id2label[label]}: {score:0.2f}'
+        ax.text(xmin, ymin, text, fontsize=15,
+                bbox=dict(facecolor='yellow', alpha=0.5))
+    plt.axis('off')
+    plt.show()
 
 def get_collator(model_name_or_path):
     processor = AutoProcessor.from_pretrained(model_name_or_path)
@@ -32,17 +52,17 @@ class Runner:
                  batch_size=4,
                  num_labels=1000,
                  max_steps=None,) -> None:
-        self.train_dataset, self.val_dataset, self.test_dataset = \
+        self.train_data, self.val_data, self.test_data = \
             EXISTING_DATASET[dataset[0]](**dataset[1])
 
         processor, self.model_processor = get_collator(model_name_or_path)
 
         self.train_dataset = DataLoader(
-            self.train_dataset, collate_fn=processor, batch_size=batch_size)
+            self.train_data, collate_fn=processor, batch_size=batch_size)
         self.val_dataset = DataLoader(
             self.val_dataset, collate_fn=processor, batch_size=batch_size)
-        self.test_dataset = DataLoader(
-            self.test_dataset, collate_fn=processor, batch_size=batch_size)
+        self.test_data = DataLoader(
+            self.test_data, collate_fn=processor, batch_size=batch_size)
 
         self.model = GeneralDetectionModel(
             AutoModelForObjectDetection.from_pretrained(
@@ -83,3 +103,25 @@ class Runner:
         self.model_processor.push_to_hub(self.hf_repo_id, token=self.token)
 
         return trainer, before_output, after_output
+    
+    def run_example(self, run_set='train', index=0):
+        example = None
+        if run_set == 'train':
+            example = self.train_data[index]
+        elif run_set == 'val':
+            example = self.val_data[index]
+        elif run_set == 'test':
+            example = self.test_data[index]
+
+        image, labels = example
+        pixel_values = image.unsqueeze(0).to(self.model.device)
+
+        with torch.no_grad():
+            outputs = self.model(pixel_values=pixel_values, pixel_mask=None)
+            print("Outputs:", outputs.keys())
+
+        results = self.model_processor.post_process_object_detection(outputs,
+                                                                target_sizes=[(1024, 1024)],
+                                                                threshold=0.9)[0]
+
+        plot_results(ToPILImage()(image), results['scores'], results['labels'], results['boxes'])

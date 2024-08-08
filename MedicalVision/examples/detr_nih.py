4de912466152cd939177e1385f67fa6bc025d0df
phuvinhnguyen <phuvinhnguyen812002@gmail.com>
2024-08-08 07:01:45 +0700

    trainining model

--- MedicalVision/examples/detr_nih.py
+++ MedicalVision/examples/detr_nih.py
@@ -7,6 +7,7 @@ import torch
 
 def run(hf_id,
         token=None,
+        pretrained_model_name_or_path='facebook/detr-resnet-50',
         image_path='/kaggle/input/nih-dataset-coco-detection/mydataset/images',
         annotations_file='/kaggle/input/nih-dataset-coco-detection/mydataset/annotations.json',
         max_epochs=100,
@@ -16,9 +17,9 @@ def run(hf_id,
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     id2label = {0: "Atelectasis", 1: "Cardiomegaly", 2: "Effusion", 3: "Infiltrate", 4: "Mass", 5: "Nodule", 6: "Pneumonia", 7: "Pneumothorax"}
-    processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50")
+    processor = DetrImageProcessor.from_pretrained(pretrained_model_name_or_path)
     dataset = get_loader(image_path, processor, annotations_file=annotations_file)
-    model = Detr(dataset['dataloader'][0], dataset['dataloader'][1], lr=lr, id2label=id2label)
+    model = Detr(dataset['dataloader'][0], dataset['dataloader'][1], lr=lr, id2label=id2label, model_name=pretrained_model_name_or_path)
 
     trainer = DetectionTrainer(model, processor, max_epochs=max_epochs, device=device)
     trainer.test(dataset['dataloader'][index_test_dataset], dataset['dataset'][index_test_dataset])

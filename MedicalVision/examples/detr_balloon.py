f407fe4ee70c337cef491e31c8c01b8d649927cb
phuvinhnguyen <phuvinhnguyen812002@gmail.com>
2024-08-15 14:20:35 +0700

    balloon

--- MedicalVision/examples/detr_balloon.py
+++ MedicalVision/examples/detr_balloon.py
@@ -1,56 +1,69 @@
-from ..trainer.detection.detection import DetectionTrainer
 from ..dataset.detection.CocoDataset import get_loader
 from ..models.detection.detr import Detr
-from ..utils.visualize import test_and_visualize_model
 from transformers import DetrImageProcessor
 import torch
+from ..trainer.detection.detection import DetectionTrainer
 from ..utils.uploadReadme import write_file_in_hf_repo
+from ..utils.model import set_all_params_to_trainable, model_params
 
 def run(hf_id,
         token=None,
         pretrained_model_name_or_path='facebook/detr-resnet-50',
-        image_path='/kaggle/working/balloon/train',
-        annotations_file='/kaggle/working/balloon/train/custom_train.json',
+        train_image_path='kaggle/working/balloon/train',
+        train_annotations_file='/kaggle/input/nih-detection-dataset/annotations/train.json',
+        valid_image_path='kaggle/working/balloon/train',
+        valid_annotations_file='/kaggle/input/nih-detection-dataset/annotations/val.json',
+        test_image_path='kaggle/working/balloon/train',
+        test_annotations_file='/kaggle/input/nih-detection-dataset/annotations/val.json',
         max_epochs=100,
         batch_size=32,
         lr=1e-4,
-        index_test_dataset=2,
-        revision='no_timm',
+        dropout_rate=0.1,
+        weight_decay=1e-4,
+        pull_revision='no_timm',
+        do_train=True,
+        push_to_hub=False,
         train_full=True,
+        push_revision=None,
+        example_path='/kaggle/working/example.png',
+        visualize_threshold=0.1
         ):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     id2label = {0: 'balloon'}
+
     processor = DetrImageProcessor.from_pretrained(pretrained_model_name_or_path)
-    dataset = get_loader(image_path, processor, annotations_file=annotations_file, batch_size=batch_size)
+    train_dataset = get_loader(train_image_path, processor, annotations_file=train_annotations_file, batch_size=batch_size)
+    valid_dataset = get_loader(valid_image_path, processor, annotations_file=valid_annotations_file, batch_size=batch_size)
+    test_dataset = get_loader(test_image_path, processor, annotations_file=test_annotations_file, batch_size=batch_size)
+        
     model = Detr(
-        dataset['dataloader'][0],
-        dataset['dataloader'][1],
+        train_dataset['dataloader'][0],
+        valid_dataset['dataloader'][0],
         lr=lr,
+        dropout_rate=dropout_rate,
+        weight_decay=weight_decay,
         id2label=id2label,
         model_name=pretrained_model_name_or_path,
-        revision=revision
-        )
+        revision=pull_revision
+    )
 
     # Set all parameters trainable
-    if train_full:
-        for param in model.parameters():
-            param.requires_grad = True
+    if train_full: set_all_params_to_trainable(model)
 
-    print(f'''Model state:
-- All parameter: {sum(p.numel() for p in model.parameters())}
-- Trainable parameter: {sum(p.numel() for p in model.parameters() if p.requires_grad)}''')
+    model_params(model)
 
     trainer = DetectionTrainer(model, processor, max_epochs=max_epochs, device=device)
-    initial_result = trainer.test(dataset['dataloader'][index_test_dataset], dataset['dataset'][index_test_dataset])
-    test_and_visualize_model(dataset['dataset'][index_test_dataset], model.model, processor, image_idx=1, image_dir=image_path, device=device)
 
-    trainer.fit()
-    final_result = trainer.test(dataset['dataloader'][index_test_dataset], dataset['dataset'][index_test_dataset])
+    initial_result = trainer.test(test_dataset['dataloader'][0], test_dataset['dataset'][0])
+    trainer.visualize(train_dataset['dataset'][0], image_dir=train_image_path, threshold=visualize_threshold)
+
+    if do_train: trainer.fit()
+    final_result = trainer.test(test_dataset['dataloader'][0], test_dataset['dataset'][0])
+    trainer.visualize(train_dataset['dataset'][0], image_dir=train_image_path, threshold=visualize_threshold)
 
     validation_tracker_epoch = ''
-    if trainer.tracker:
-        validation_tracker_epoch = '\n'.join([str(i) for i in trainer.tracker[0].validation_epoch_end])
+    if trainer.trackers: validation_tracker_epoch = '\n'.join([str(i) for i in trainer.trackers[0].validation_epoch_end])
 
     commit_message = f'''---
 library_name: transformers
@@ -66,25 +79,31 @@ tags: []
 {final_result}```
 
 ## Config
-- dataset: Balloon
+- dataset: NIH
 - original model: {pretrained_model_name_or_path}
 - lr: {lr}
+- dropout_rate: {dropout_rate}
+- weight_decay: {weight_decay}
 - max_epochs: {max_epochs}
+- train samples: {len(train_dataset['dataset'][0])}
 
 ## Logging
 ### Training process
 ```
 {validation_tracker_epoch}
 ```
+
+## Examples
+{train_dataset['examples']}
+
+![Example](./example.png)
 '''
     with open('./README.md', 'w') as wf:
         wf.write(commit_message)
 
-    model.model.push_to_hub(hf_id, token=token, commit_message=commit_message)
-    processor.push_to_hub(hf_id, token=token)
-
-    write_file_in_hf_repo('./README.md', hf_id, token)
-
-    test_and_visualize_model(dataset['dataset'][index_test_dataset], model.model, processor, image_idx=1, image_dir=image_path, device=device)
+    if push_to_hub:
+        trainer.push_to_hub(hf_id, token, revision=push_revision)
+        write_file_in_hf_repo('./README.md', hf_id, token, revision=push_revision)
+        write_file_in_hf_repo(example_path, hf_id, token, revision=push_revision, desfilename='example.png')
 
     return model
---
+++ kaggle/Balloonmaker.py
@@ -0,0 +1,32 @@
+!wget https://github.com/matterport/Mask_RCNN/releases/download/v2.1/balloon_dataset.zip
+!unzip balloon_dataset.zip > /dev/null
+
+!git clone https://github.com/woctezuma/VIA2COCO
+%cd VIA2COCO/
+!git checkout fixes
+
+import convert as via2coco
+
+data_path = '/kaggle/working/balloon/'
+
+first_class_index = 0
+
+for keyword in ['train', 'val']:
+    input_dir = data_path + keyword + '/'
+    input_json = input_dir + 'via_region_data.json'
+    categories = ['balloon']
+    super_categories = ['N/A']
+    output_json = input_dir + 'custom_' + keyword + '.json'
+
+    print('Converting {} from VIA format to COCO format'.format(input_json))
+
+    coco_dict = via2coco.convert(
+        imgdir=input_dir,
+        annpath=input_json,
+        categories=categories,
+        super_categories=super_categories,
+        output_file_name=output_json,
+        first_class_index=first_class_index,
+    )
+
+%cd ..
